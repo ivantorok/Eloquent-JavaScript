eb02c367631adb8bdf66e0506dd8e314017cf21a
ivan.torok <ivan.torok@gmail.com>
2015-12-15 10:33:25 +0100

    Question in Chatper05.js between line 101 and 167

--- Chapter05.js
+++ Chapter05.js
@@ -97,3 +97,71 @@ The cost
 Great-great-great-great-...
 2015-11-04 09:53:15 Budapest Time: to be continued from here...
 */
+
+/*2015-12-15 10:26:36 Budapest Time: I do not understand the "/ 4" part at the end of the sample code here:
+http://eloquentjavascript.net/05_higher_order.html
+###############################################################################
+
+Great-great-great-great-...
+
+My grandfather, Philibert Haverbeke, is included in the data file. By starting with him, I can trace my lineage to find out whether the most ancient person in the data, Pauwels van Haverbeke, is my direct ancestor. And if he is, I would like to know how much DNA I theoretically share with him.
+
+To be able to go from a parent’s name to the actual object that represents this person, we first build up an object that associates names with people.
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+console.log(byName["Philibert Haverbeke"]);
+// → {name: "Philibert Haverbeke", …}
+Now, the problem is not entirely as simple as following the father properties and counting how many we need to reach Pauwels. There are several cases in the family tree where people married their second cousins (tiny villages and all that). This causes the branches of the family tree to rejoin in a few places, which means I share more than 1/2G of my genes with this person, where G for the number of generations between Pauwels and me. This formula comes from the idea that each generation splits the gene pool in two.
+
+A reasonable way to think about this problem is to look at it as being analogous to reduce, which condenses an array to a single value by repeatedly combining values, left to right. In this case, we also want to condense our data structure to a single value but in a way that follows family lines. The shape of the data is that of a family tree, rather than a flat list.
+
+The way we want to reduce this shape is by computing a value for a given person by combining values from their ancestors. This can be done recursively: if we are interested in person A, we have to compute the values for A’s parents, which in turn requires us to compute the value for A’s grandparents, and so on. In principle, that’d require us to look at an infinite number of people, but since our data set is finite, we have to stop somewhere. We’ll allow a default value to be given to our reduction function, which will be used for people who are not in the data. In our case, that value is simply zero, on the assumption that people not in the list don’t share DNA with the ancestor we are looking at.
+
+Given a person, a function to combine values from the two parents of a given person, and a default value, reduceAncestors condenses a value from a family tree.
+
+function reduceAncestors(person, f, defaultValue) {
+  function valueFor(person) {
+    if (person == null)
+      return defaultValue;
+    else
+      return f(person, valueFor(byName[person.mother]),
+                       valueFor(byName[person.father]));
+  }
+  return valueFor(person);
+}
+The inner function (valueFor) handles a single person. Through the magic of recursion, it can simply call itself to handle the father and the mother of this person. The results, along with the person object itself, are passed to f, which returns the actual value for this person.
+
+We can then use this to compute the amount of DNA my grandfather shared with Pauwels van Haverbeke and divide that by four.
+
+SAMPLE CODE COMES HERE
+###############################################################################
+function sharedDNA(person, fromMother, fromFather) {
+  if (person.name == "Pauwels van Haverbeke")
+    return 1;
+  else
+    return (fromMother + fromFather) / 2;
+}
+var ph = byName["Philibert Haverbeke"];
+console.log(reduceAncestors(ph, sharedDNA, 0) / 4);
+// → 0.00049
+###############################################################################
+THE TEXT CONTIUES LIKE THIS:
+
+The person with the name Pauwels van Haverbeke obviously shared 100 percent of his DNA with Pauwels van Haverbeke (there are no people who share names in the data set), so the function returns 1 for him. All other people share the average of the amounts that their parents share.
+
+So, statistically speaking, I share about 0.05 percent of my DNA with this 16th-century person.
+
+MY QUESTION IS THIS: WHY DO WE NEED THE "/ 4" AT THE END OF LINE 8?
+IF I CHANGE THE ph VALUE TO 
+byName["Pauwels van Haverbeke"]
+THEN I GET THE RESULT 0.25
+AS FAR AS I UNDERSTAND I SHOULD GET 1 HERE, AS "Pauwels van Haverbeke" SHARES 100% OF HIS OWN DNA.
+
+Could you please advise?
+
+###############################################################################
+*/
